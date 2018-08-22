73af78d3bb0bebd6290a8d1640bd85bd5efe3137
hungphan <colorvisavn@gmail.com>
2018-08-22 16:08:19 +0800

    Allow to do graceful shutdown

--- src/main.go
+++ src/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"github.com/hung-phan/chat-app/src/application"
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
 	"github.com/hung-phan/chat-app/src/infrastructure"
+	"github.com/hung-phan/chat-app/src/application"
 )
 
 func main() {
+	go infrastructure.StartHTTPServer(":3000", application.CreateRouter())
 	go infrastructure.StartTCPServer(":3001", application.HandleIncomingMessage)
-	infrastructure.StartHTTPServer(":3000", application.CreateRouter())
+
+	gracefulStop := make(chan os.Signal)
+
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+
+	sig := <-gracefulStop
+
+	fmt.Printf("caught sig: %+v", sig)
+
+	os.Exit(0)
 }
