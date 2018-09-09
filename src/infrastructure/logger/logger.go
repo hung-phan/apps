e4635e2821c663426eaa09e57e035e8f7df171b9
hungphan <colorvisavn@gmail.com>
2018-09-09 21:48:46 +0800

    Use simple logger initialization

--- src/infrastructure/logger/logger.go
+++ src/infrastructure/logger/logger.go
@@ -1,23 +1,19 @@
 package logger
 
 import (
-	"github.com/hung-phan/chat-app/src/common/singleflight"
 	"go.uber.org/zap"
 )
 
 var (
-	group  = singleflight.Group{}
 	Client = createLogger()
 )
 
 func createLogger() *zap.Logger {
-	logger, err := group.Do("GetLogger", func() (interface{}, error) {
-		return zap.NewProduction()
-	})
+	logger, err := zap.NewProduction()
 
 	if err != nil {
 		panic(err)
 	}
 
-	return logger.(*zap.Logger)
+	return logger
 }
